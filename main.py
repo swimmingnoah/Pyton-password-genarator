4a3b2a6c68ea0146414153c18e2d85ac706cd239
swimmingnoah <noah@nemecfamily.com>
2021-04-30 12:09:35 -0500

    added writeItems

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import random
 import string
+import csv
 
 
 
 def password_input():
-    website = input("What website you would like to create a new password for? ")
+    website = input(
+        "What website you would like to create a new password for? ")
     email = input("What email would you like to use? ")
+
     while True:
         try:
             digit = int(input("How long would you like your password to be? "))
+            myTuple = (digit, website, email)
+            return myTuple
         except ValueError:
             print("Please enter an integer.")
             continue
-        else:
-            return digit
-            break
-    
 
 
 def password_creator(digit):
     password_characters = string.ascii_letters + string.digits
     password = ''.join(random.choice(password_characters)
                        for i in range(digit))
+    return password
+
 
 def password_print(website, email, password):
     print("\nYour password is:")
@@ -29,10 +32,20 @@ def password_print(website, email, password):
     print("\nEmail: " + email)
     print("\nPassword: " + password)
 
+
 def write_password():
     f = open("super_secret_passwords.txt", "r")
     f.write(digit)
     f.close()
+
+
+def writeItems(website, email, password):
+    vault = 'super_secret_passwords.txt'
+    with open(vault, mode='a+') as vault:
+        vault_writer = csv.writer(
+            vault, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        vault_writer.writerow([website, email, password])
+
 
 def main():
 
@@ -43,10 +56,13 @@ def main():
     option_choice = int(input("Please enter a choice: "))
 
     if option_choice == 1:
-        password_input()
-        write_password()
-        print(website)
-
+        x = password_input()
+        digit = x[0]
+        website = x[1]
+        email = x[2]
+        password = password_creator(x[0])
+        password_print(website, email, password)
+        writeItems(website, email, password)
 
     elif option_choice == 2:
         view_pass()
@@ -54,5 +70,5 @@ def main():
     else:
         print("please pick a number between 1 and 3")
 
+
 main()
-print(website)
